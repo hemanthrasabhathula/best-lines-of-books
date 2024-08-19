f094353bab325872eaa9d421bdf1098efb83226c
Hemanth Rasabhathula <hemanthrasabhathula@gmail.com>
2024-08-19 14:59:38 -0500

    refactor: Update PromptService to use environment variables for API URL and Key

--- src/Services/PromptService.ts
+++ src/Services/PromptService.ts
@@ -1,12 +1,14 @@
-import { GROQ_API_URL } from "../constants/Constants";
 import { AIAPIRequest } from "../types/AIAPIRequest";
 
 export const fetchPromptResponse = async (
   data: AIAPIRequest
 ): Promise<string> => {
   try {
+    const groq_api_url = process.env.REACT_APP_GROQ_API_URL;
     const groq_api_key = process.env.REACT_APP_GROQ_API_KEY;
-    const response = await fetch(GROQ_API_URL, {
+    if (!groq_api_url) throw new Error("API URL not found");
+    if (!groq_api_key) throw new Error("API Key is missing");
+    const response = await fetch(groq_api_url, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
--- src/constants/Constants.ts
+++ src/constants/Constants.ts
@@ -1,24 +1,12 @@
-export const API_ENDPOINT = "http://localhost:5000/api";
+export const API_ENDPOINT =
+  process.env.REACT_APP_API_ENDPOINT || "http://localhost:5000/api";
 
 export const BOOKS_LIST = "bookslist";
-export const BRANCHES_LIST = "brancheslist";
-export const CART_ITEMS = "cartitems";
-export const TIMESTAMP = "timestamp";
+
 export const BOOKS = "books";
-export const BRANCHES = "branches";
-export const CART = "cart";
-export const BOOKS_URL = `${API_ENDPOINT}/books`;
-export const BRANCHES_URL = `${API_ENDPOINT}/branches`;
-export const CART_URL = `${API_ENDPOINT}/cart`;
-export const GROQ_API_URL = "https://api.groq.com/openai/v1/chat/completions";
 
 export const BOOK_INFO_PROMPT =
   "Provide the book details for :your-book-title in a single-line JSON format without any extra text or newline characters. The JSON should include fields: title, author, ISBN (in the format 123-123456789), genre (comma seperated string), published (only year), pages,image and description (The description should be a brief paragraph giving a glimpse of what the book is about, with at least 6 lines and no more than 8 lines should not include special characters).";
-
-export const SESSION_KEY = "userSession";
-
-const fifteen_min = 1000 * 60 * 15;
-export const SESSION_TIMEOUT = fifteen_min;
 
 export const colors = [
   "#FFDFD6",
